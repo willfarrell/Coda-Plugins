cfe00a830377e720f0861dba88e6be1bc94c1ac6
will Farrell <will.farrell@gmail.com>
2011-12-04 16:09:09 -0500

    Bug Fix for RemoveUselessFiles

--- RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
+++ RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [ "$CODA_SITE_LOCAL_PATH" != "" ]; then
 	cd "$CODA_SITE_LOCAL_PATH"
-	find . -name '*.DS_Store' -type f -delete
-    find . -name '_notes' -type f -delete
-    find . -regex '^core.[0-9]{4,}$' -type f -delete
+	find . -name '*.DS_Store' -exec rm -rf '{}' \;
+    find . -name '_notes' -exec rm -rf '{}' \;
+    find . -name 'error_log' -exec rm -rf '{}' \;
+    find . -regex '^core.[0-9]{4,}$' -exec rm -rf '{}' \;
 fi
 exit 0
