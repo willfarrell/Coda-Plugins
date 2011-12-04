f39aaa0430377628a16183260031ebaf5350dc4e
will Farrell <will.farrell@gmail.com>
2011-12-04 16:11:07 -0500

    added the removal of Thumbs.db files to RemoveUselessFiles Plugin

--- RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
+++ RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
@@ -2,6 +2,7 @@
 if [ "$CODA_SITE_LOCAL_PATH" != "" ]; then
 	cd "$CODA_SITE_LOCAL_PATH"
 	find . -name '*.DS_Store' -exec rm -rf '{}' \;
+    find . -name 'Thumbs.db' -exec rm -rf '{}' \;
     find . -name '_notes' -exec rm -rf '{}' \;
     find . -name 'error_log' -exec rm -rf '{}' \;
     find . -regex '^core.[0-9]{4,}$' -exec rm -rf '{}' \;
