2b185c9029194a4bb35cc832e8e1f91fcf4688ac
will Farrell <will.farrell@gmail.com>
2012-10-11 10:36:45 -0400

    code clean up

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Plugins
 
 ## Remove Useless Files ##
+* Removes Thumbs.db files
 * Removes .DS_Store files
+* Removes ._whatever files
 * Removes error_log files
 * Removes core.0000 files
 * Removes _notes folders
--- RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
+++ RemoveUselessFiles.codaplugin/Contents/Resources/4A09F366-EE3A-40F1-8639-95935C6E23DE/remove.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 if [ "$CODA_SITE_LOCAL_PATH" != "" ]; then
-	cd "$CODA_SITE_LOCAL_PATH"
-	find . -name '*.DS_Store' -exec rm -rf '{}' \;
-    find . -name 'Thumbs.db' -exec rm -rf '{}' \;
-    find . -name '_notes' -exec rm -rf '{}' \;
-    find . -name 'error_log' -exec rm -rf '{}' \;
-    find . -regex '^core.[0-9]{4,}$' -exec rm -rf '{}' \;
+    cd "$CODA_SITE_LOCAL_PATH"
+    # http://www.westwind.com/reference/os-x/invisibles.html
 
-    // as per request
-    find . -regex '^\._(.*)$' -exec rm -rf '{}' \;
+    find . -name 'Thumbs.db' -exec rm -rf {} \; # Windows
+    find . -name '.DS_Store' -exec rm -rf {} \; # Mac OSX
+    find . -name '._*' -exec rm -rf {} \; # Mac OSX resource forks
+
+    find . -name '_notes' -exec rm -rf {} \; # Dreamweaver
+    #find . -name '.ceid' -exec rm -rf {} \; # PogoPlug
+    #find . -name '.cedata' -exec rm -rf {} \; # PogoPlug
+    
+    find . -name 'error_log' -exec rm -rf {} \; # Apache via MAMP
+    find . -regex '^core.[0-9]{4,}$' -exec rm -rf {} \; # core dumps
 fi
 exit 0
